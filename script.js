a5ecdcd2241afad232d5ec0f9f38b3dd4649479d
Karine Souza <kszmr@hotmail.com>
2020-11-22 01:30:48 -0300

    readonly inputs and hide arrows

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function calculate () {
     let a = Number(document.getElementById("number-a").value);
     let b = Number(document.getElementById("number-b").value);
-    add(a, b);
-    sub1(a, b);
-
+    if (a == "" || b == "") {
+        alert("You should put two numbers!")
+    } else {
+        add(a, b);
+        sub1(a, b);
+        sub2(a, b);
+        multiply(a, b);
+        div1(a, b);
+        div2(a, b);
+        square1(a, b);
+        square2(a, b);
+    }
 }
 
 function add (a, b) {
@@ -12,8 +21,43 @@ function add (a, b) {
 }
 
 function sub1 (a, b) {
-    let sub1 = a - b;
-    document.getElementById("sub1-result").value = sub1;
+    let subOneResult = a - b;
+    document.getElementById("sub1-result").value = subOneResult;
+}
+
+function sub2 (a, b) {
+    let subTwoResult = b - a;
+    document.getElementById("sub2-result").value = subTwoResult;
+}
+
+function multiply (a, b) {
+    let multi = a * b;
+    document.getElementById("multi-result").value = multi;
+}
+
+function div1 (a, b) {
+    let divOneResult = a / b;
+    document.getElementById("div1-result").value = divOneResult;
+}
+
+function div2 (a, b) {
+    let divTwoResult = b / a;
+    document.getElementById("div2-result").value = divTwoResult;
+}
+
+function square1 (a, b) {
+    let squareOneResult = a * a;
+    document.getElementById("square-a-result").value = squareOneResult;
+}
+
+function square2 (a, b) {
+    let squareTwoResult = b * b;
+    document.getElementById("square-b-result").value = squareTwoResult;
 }
 
 document.getElementById("calculate").addEventListener("click", calculate);
+document.getElementById("number-b").addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        calculate();
+    }
+});
